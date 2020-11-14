9e86a3102c926e19d8297871222513971e15682c
Felipe Ximenes <ximenesfel@gmail.com>
2020-11-14 18:57:01 -0300

    fix: run prettier formater

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/ximenesfel/react_github_actions",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/ximenesfel/react_github_actions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
