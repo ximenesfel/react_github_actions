4c27160dc496de208c4000a9c091bb4c7e31a7c3
Felipe Ximenes <ximenesfel@gmail.com>
2020-11-14 18:52:37 -0300

    Install semantic-release package

---
+++ release.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/ximenesfel/react_github_actions",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
+}
